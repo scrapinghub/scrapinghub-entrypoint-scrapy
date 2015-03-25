74766694f13efeada2e12f0c9e01b59f32ff2a61
Daniel Graña <dangra@gmail.com>
2015-03-25 00:54:15 -0300

    update stats module

--- sh_scrapy/stats.py
+++ sh_scrapy/stats.py
@@ -20,17 +20,18 @@ class HubStorageStatsCollector(StatsCollector):
         row = [int(time.time() * 1000)]
         row.extend(self._stats.get(x, 0) for x in self.STATS_TO_COLLECT)
         hsref.job.samples.write(row)
+        hsref.job.metadata.apipost(jl={'scrapystats': self._stats})
 
     def open_spider(self, spider):
+        self._setup_looping_call(now=True)
+
+    def _setup_looping_call(self, _ignored=None, **kwargs):
         self._samplestask = task.LoopingCall(self._upload_stats)
-        self._samplestask.start(self.INTERVAL, now=True)
+        d = self._samplestask.start(self.INTERVAL, **kwargs)
+        d.addErrback(self._setup_looping_call, now=False)
 
     def close_spider(self, spider, reason):
         super(HubStorageStatsCollector, self).close_spider(spider, reason)
         if self._samplestask.running:
             self._samplestask.stop()
         self._upload_stats()
-
-    def _persist_stats(self, stats, spider):
-        hsref.job.metadata['scrapystats'] = stats
-        hsref.job.metadata.save()
