58b648c75666ac7beffd24d566dc9a4f9749ab88
Daniel Graña <dangra@gmail.com>
2016-04-27 03:48:48 -0300

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapinghub-entrypoint-scrapy',
-    version='0.5.0',
+    version='0.6.0',
     packages=find_packages(),
     install_requires=[
         'Scrapy>=1.0',
