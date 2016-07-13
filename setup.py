ae85378e3fb97dd3159bada21b3a1a8f31ad17a1
Viktor Shlapakov <vshlapakov@gmail.com>
2016-07-13 15:17:29 +0300

    Bump version: 0.7.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapinghub-entrypoint-scrapy',
-    version='0.7.0',
+    version='0.8.0',
     license='BSD',
     description='Scrapy entrypoint for Scrapinghub job runner',
     long_description=open('README.rst').read(),
