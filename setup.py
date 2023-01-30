a55cc0645da9f45342dc042d5f9fdab15e6d3071
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2023-01-30 11:54:12 -0300

    Bump version: 0.13.0 → 0.14.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapinghub-entrypoint-scrapy',
-    version='0.13.0',
+    version='0.14.0',
     license='BSD',
     description='Scrapy entrypoint for Scrapinghub job runner',
     long_description=open('README.md').read(),
