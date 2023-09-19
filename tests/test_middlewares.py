baa44fbda371e9ebf383d4b1007a4c79724f23ad
Shevchenko Taras <stgmont@gmail.com>
2023-09-19 18:56:59 +0300

    update request's type
    Co-authored-by: Eugenio Lacuesta <1731933+elacuesta@users.noreply.github.com>

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -88,7 +88,7 @@ def test_hs_middlewares_dummy_response(hs_downloader_middleware, hs_spider_middl
     class DummyResponse(Response):
         __module__: str = "scrapy_poet.api"
 
-        def __init__(self, url: str, request=Optional[Request]):
+        def __init__(self, url: str, request: Optional[Request] = None):
             super().__init__(url=url, request=request)
 
     spider = Spider('test')
