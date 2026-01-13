98fd4bdffb2661855e0d34548e225e0e042bec65
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2026-01-13 13:23:46 -0300

    Update DiskQuota middlewares

--- sh_scrapy/diskquota.py
+++ sh_scrapy/diskquota.py
@@ -3,33 +3,58 @@ DiskQuota downloader and spider middlewares.
 The goal is to catch disk quota errors and stop spider gently.
 """
 
+from scrapy import Spider
+from scrapy.crawler import Crawler
 from scrapy.exceptions import NotConfigured
+from scrapy.http import Request, Response
+
+from sh_scrapy import _SCRAPY_ASYNC_API
 
 
-class DiskQuota(object):
+class DiskQuota:
 
-    def __init__(self, crawler):
-        if not crawler.settings.getbool('DISK_QUOTA_STOP_ON_ERROR'):
+    def __init__(self, crawler: Crawler) -> None:
+        if not crawler.settings.getbool("DISK_QUOTA_STOP_ON_ERROR"):
             raise NotConfigured
         self.crawler = crawler
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler) -> "DiskQuota":
         return cls(crawler)
 
-    def _is_disk_quota_error(self, error):
+    def _is_disk_quota_error(self, error: Exception) -> bool:
         return isinstance(error, (OSError, IOError)) and error.errno == 122
 
 
 class DiskQuotaDownloaderMiddleware(DiskQuota):
 
-    def process_exception(self, request, exception, spider):
-        if self._is_disk_quota_error(exception):
-            self.crawler.engine.close_spider(spider, 'diskusage_exceeded')
+    if _SCRAPY_ASYNC_API:
+
+        def process_exception(self, request: Request, exception: Exception) -> None:
+            if self._is_disk_quota_error(exception):
+                self.crawler.engine.close_spider(self.crawler.spider, reason="diskusage_exceeded")
+
+    else:
+
+        def process_exception(
+            self, request: Request, exception: Exception, spider: Spider
+        ) -> None:
+            if self._is_disk_quota_error(exception):
+                self.crawler.engine.close_spider(spider, "diskusage_exceeded")
 
 
 class DiskQuotaSpiderMiddleware(DiskQuota):
 
-    def process_spider_exception(self, response, exception, spider):
-        if self._is_disk_quota_error(exception):
-            self.crawler.engine.close_spider(spider, 'diskusage_exceeded')
+    if _SCRAPY_ASYNC_API:
+
+        def process_spider_exception(self, response: Response, exception: Exception) -> None:
+            if self._is_disk_quota_error(exception):
+                self.crawler.engine.close_spider(self.crawler.spider, "diskusage_exceeded")
+
+    else:
+
+        def process_spider_exception(
+            self, response: Response, exception: Exception, spider: Spider
+        ) -> None:
+            if self._is_disk_quota_error(exception):
+                self.crawler.engine.close_spider(spider, "diskusage_exceeded")
